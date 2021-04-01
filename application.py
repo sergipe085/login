f488b1194bdfda5ba3eaa792a047bb05fc396712
serjolas1 <dapscreed@gmail.com>
2021-04-01 14:58:29 -0300

    session configuration

--- application.py
+++ application.py
@@ -1,1 +1,15 @@
-   
+from flask import Flask, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html') 
