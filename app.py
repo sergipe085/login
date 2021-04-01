2ec4f4de753107a00b280bcbdba6fedb75d58117
serjolas1 <dapscreed@gmail.com>
2021-04-01 15:07:00 -0300

    login with sections

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, redirect, request, session
 from flask_session import Session
 
 app = Flask(__name__)
@@ -10,6 +10,9 @@ Session(app)
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        return redirect('/')
     return render_template('login.html') 
