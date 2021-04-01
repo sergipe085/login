567272e8b693ec6cc2cac19b372ad472d14bf734
serjolas1 <dapscreed@gmail.com>
2021-04-01 15:09:05 -0300

    logout

--- app.py
+++ app.py
@@ -16,3 +16,8 @@ def login():
         session["name"] = request.form.get("name")
         return redirect('/')
     return render_template('login.html') 
+
+@app.route('/logout')
+def logout():
+    session["name"] = None
+    return redirect('/')
